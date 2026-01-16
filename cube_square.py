c64f92c01371b5aea14df7acd7868b6318a01f00
Shreya <shreya1913@gmail.com>
2026-01-16 16:44:11 +0530

    third commit

--- cube_square.py
+++ cube_square.py
@@ -1,12 +1,13 @@
+import sys
 def cube_square(num):
-    cube = num ** 3
     square = num ** 2
+    cube = num ** 3
     return square, cube
 
 if __name__ == "__main__":
+    sys.argv[cube_square]
     num = int(input("Enter a number: "))
     square, cube = cube_square(num)
     print("Square:", square)
     print("Cube:", cube)
 
-       
